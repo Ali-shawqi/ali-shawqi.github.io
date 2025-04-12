002bb19e3086cd602f2e94ce65413be37a184d30
Ali-shawqi <aashawqi@my.waketech.edu>
2025-04-11 21:09:42 -0400

    added gradebook

---
+++ gradebook.js
@@ -0,0 +1,10 @@
+function fetchGradeData() {
+	console.log("Fetching Grade data...")
+	}
+	
+function populateGradebook(data) {
+	console.log("populating gradebook with data:", data);
+	}
+
+const gradeData=fetchGradeData();
+populateGradebook(gradeData);
